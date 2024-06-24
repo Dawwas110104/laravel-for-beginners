625a79542f86adef7ac6f0c0764127c6452e8ba8
dawwas110104 <dawwas.inha@gmail.com>
2024-06-25 00:06:17 +0700

    Add first page

---
+++ src/app/laravel-for-beginners/contents.js
@@ -0,0 +1,21 @@
+export const contents  = [
+    {
+        title: "Getting Started",
+        path: "/laravel-for-beginners",
+        submenu: true,
+        submenuItems : [
+            { title: "Installation", path: "/laravel-for-beginners/getting-started/installation"},
+        ],
+    },
+    {
+        title: "Routes",
+        path: "/laravel-for-beginners/routes/",
+        submenu: true,
+        submenuItems : [
+            { title: "Route Introduction", path: "/laravel-for-beginners/routes/intro"},
+            { title: "Route Part 1", path: "/laravel-for-beginners/routes/part-1"},
+            { title: "Route Part 2", path: "/laravel-for-beginners/routes/part-2"},
+            { title: "Naming Routes", path: "/laravel-for-beginners/routes/naming-routes"},
+        ],
+    },    
+]
